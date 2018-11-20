504b16524421376d6e9e37bf440a2769c7c56f8f
ericturf <eric.turriff@gmail.com>
2018-11-20 10:36:43 -0500

    Added preset naming

--- src/components/dialogs/index.js
+++ src/components/dialogs/index.js
@@ -1,2 +1,3 @@
 export { default as AddPlanetDialog } from './add-planet/dialog';
 export { default as SettingsDialog } from './settings/dialog';
+export { default as RenamePresetDialog } from './rename-preset/dialog';
